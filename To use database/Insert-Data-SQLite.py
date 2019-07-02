4a874ef3ad0dbbeba55c0343207142c11e173893
unknown <koyasairammanohar@gmail.com>
2019-07-02 14:27:12 +0530

    Stemmed tags

--- To use database/Insert-Data-SQLite.py
+++ To use database/Insert-Data-SQLite.py
@@ -6,16 +6,20 @@ import nltk
 import sqlite3
 import csv
 from queryInfo import QueryInfo
+from nltk.stem import PorterStemmer 
 
+ps = PorterStemmer()
 
 conn = sqlite3.connect('example.db')
 cur = conn.cursor()
-# cur.execute("""create table infoData 
-#             (title text,
-#             view_ct integer,
-#             ans_ct integer,
-#             fav_ct integer,
-#             tags text)""")
+
+def createTable():
+    cur.execute("""create table infoData 
+            (title text,
+            view_ct integer,
+            ans_ct integer,
+            fav_ct integer,
+            tags text)""")
 
 def retTags(Title):   
     A=-1  
@@ -23,7 +27,7 @@ def retTags(Title):
     taggedP = nltk.pos_tag(words)
     Realtags = []
     for (a,b) in taggedP:
-        if(b=='NNP') : 
+        if(b=='NNP' or b.startswith('VB')) : 
             Realtags.append(a)
             A=0
     if(A==-1): 
@@ -31,29 +35,30 @@ def retTags(Title):
             if(b=='NN'): 
                 Realtags.append(a)
                 A=0
+    rtags = [ps.stem(w) for w in Realtags]
     st = " "
-    return st.join(Realtags)
+    return st.join(rtags)
 
 def pushObj(obj,tags):
     t = (obj.title,obj.ViewCt,obj.AnsCt,obj.FavCt,tags)
     cur.execute("insert into infoData values (?,?,?,?,?)",t)
     
 
-# with open("ShapeData-50K.csv",'r',encoding='utf-8') as csvFile:
-#     reader = csv.reader(csvFile)
-#     for [a,b,c,d,e] in reader:
-#         obj = QueryInfo(b,c,d,e)
-#         tags = retTags(b)
-#         # print(tags)
-#         pushObj(obj,tags)
-        
 
-
+def csvToDB():
+    with open("ShapeData-50K.csv",'r',encoding='utf-8') as csvFile:
+        reader = csv.reader(csvFile)
+    for [a,b,c,d,e] in reader:
+        obj = QueryInfo(b,c,d,e)
+        tags = retTags(b)
+        # print(tags)
+        pushObj(obj,tags)
 
 
 cur.execute("""
-                select * from infoData where ans_ct=50
+              select * from infoData where fav_ct=3
             """)
+
 
 print(cur.fetchall())
 conn.commit()
